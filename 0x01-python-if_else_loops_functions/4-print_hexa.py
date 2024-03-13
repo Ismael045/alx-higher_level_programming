4cc850bd201a2c19eeda7a5566196d77633f8eb6
Ismael045 <adamismael297@gmail.com>
2024-03-12 18:55:27 -0700

    Update

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for number in range(0,99):
-    print('{} = 0v{:x}'.format(num, num))
+
+for number in range(0, 99):
+    print("{} = 0x{:x}".format(number, number))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-# Python program that:
-# demonstrates how to print numbers from 0 to 99
-# demonstrates how to use a loop and string formatting to affect program output
-#
-
 for number in range(0, 99):
-
-    print("{:02d}, ".format(number), end=", ")
-
+    print("{:02d}, ".format(number), end="")
 print("99")
 
